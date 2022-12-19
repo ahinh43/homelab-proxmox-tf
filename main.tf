f2a533b967d715bfab659862a034e53153c05978
Alec Hinh <alec4555@gmail.com>
2022-12-19 12:40:59 -0600

    Combine disks together and remove one lxc

--- main.tf
+++ main.tf
@@ -64,13 +64,9 @@ module "kube_worker_1" {
   kubernetes_cacert_hash             = var.k8s_cacert_hash
   additional_disk_configurations = [
     {
-      size = "40G"
+      size = "100G"
       storage_name = "local-lvm"
-    },
-    {
-      size = "60G"
-      storage_name = "local-lvm"
-    } 
+    }
   ]
 }
 
@@ -88,13 +84,9 @@ module "kube_worker_2" {
   kubernetes_cacert_hash             = var.k8s_cacert_hash
   additional_disk_configurations = [ 
     {
-      size = "40G"
+      size = "100G"
       storage_name = "grace-lvm"
-    },
-    {
-      size = "60G"
-      storage_name = "grace-lvm"
-    } 
+    }
   ]
 }
 module "kube_worker_3" {
@@ -111,39 +103,10 @@ module "kube_worker_3" {
   kubernetes_cacert_hash             = var.k8s_cacert_hash
   additional_disk_configurations = [ 
     {
-      size = "40G"
+      size = "100G"
       storage_name = "local-lvm-thin"
-    },
-    {
-      size = "60G"
-      storage_name = "local-lvm-thin"
-    } 
+    }
   ]
-}
-
-
-module "nikkos_pizza_server" {
-  source                   = "./modules/standard-vms/lxc-container"
-  name                     = "nikkocraft"
-  target_node              = "shizuru"
-  clone_storage            = "local-lvm"
-  ssh_private_key          = var.ssh_private_key
-  template_vmid            = "112"
-  ip_address               = "10.1.1.12"
-  cpu_cores                = 4
-  memory                   = 18432
-  root_disk_size           = 40
-  provision_minecraft      = true
-  minecraft_server_type    = "forge"
-  minecraft_server_version = "1.12.2"
-  minecraft_jre_version    = "8"
-  minecraft_jre_min_mem    = "2"
-  minecraft_jre_max_mem    = "7"
-}
-
-moved {
-  from = module.nikkos_pizza_server.null_resource.minecraft
-  to   = module.nikkos_pizza_server.null_resource.minecraft[0]
 }
 
 module "pihole_dns_server_2" {
