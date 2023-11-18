ada3e4b286c47990ad294c67d9c1b269a0ee30fd
Alec Hinh <alec4555@gmail.com>
2023-11-17 21:14:25 -0600

    Add kube2 controller 2

--- main.tf
+++ main.tf
@@ -41,6 +41,55 @@ module "kube2_controller_primary" {
   cloudflare_zone_id = var.cloudflare_zone_id
 }
 
+module "kube2_controller_2" {
+  source                             = "./modules/kubernetes-vms"
+  vm_name                            = "kube2cont02"
+  vm_ip_address                      = "10.1.1.41"
+  target_node                        = "lefi"
+  template_id                        = 109
+  vm_cpu_sockets                     = 1
+  vm_cpu_cores                       = 6
+  vm_memory                          = 14336
+  ssh_private_key                    = var.ssh_private_key
+  kubernetes_type                    = "controller"
+  kubernetes_cluster_token           = var.k8s_cluster_information[1].cluster_token
+  kubernetes_cluster_certificate_key = var.k8s_cluster_information[1].certificate_key
+  kubernetes_cacert_hash             = var.k8s_cluster_information[1].cacert_hash
+  additional_disk_configurations = [
+    {
+      size         = 100
+      storage_name = "local-lvm"
+    }
+  ]
+  make_controller_worker = true
+  cloudflare_zone_id     = var.cloudflare_zone_id
+}
+
+# module "kube2_controller_3" {
+#   source                             = "./modules/kubernetes-vms"
+#   vm_name                            = "kube2cont03"
+#   vm_ip_address                      = "10.1.1.42"
+#   target_node                        = "vern"
+#   template_id                        = 112
+#   vm_cpu_sockets                     = 1
+#   vm_cpu_cores                       = 6
+#   vm_memory                          = 14336
+#   ssh_private_key                    = var.ssh_private_key
+#   kubernetes_type                    = "controller"
+#   kubernetes_cluster_token           = var.k8s_cluster_information[1].cluster_token
+#   kubernetes_cluster_certificate_key = var.k8s_cluster_information[1].certificate_key
+#   kubernetes_cacert_hash             = var.k8s_cluster_information[1].cacert_hash
+#   additional_disk_configurations = [
+#     {
+#       size         = 100
+#       storage_name = "local-lvm"
+#     }
+#   ]
+#   make_controller_worker = true
+#   cloudflare_zone_id     = var.cloudflare_zone_id
+# }
+
+
 
 # Note that bulk creating all the nodes at once brings a problem in regards to the timing of a VM's creation
 # and the assignment of a DHCP IP. A VM may accidentally target a DHCP IP that is assigned to the unintended target
