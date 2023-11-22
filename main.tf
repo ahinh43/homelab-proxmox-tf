0cd5c9b59e542bebbd691270c1e9599c25836429
Alec Hinh <alec4555@gmail.com>
2023-11-21 22:33:09 -0600

    Enable kube2

--- main.tf
+++ main.tf
@@ -125,80 +125,80 @@ module "kube_worker_3" {
   cloudflare_zone_id = var.cloudflare_zone_id
 }
 
-# module "kube2_controller_primary" {
-#   source                       = "./modules/kubernetes-vms"
-#   vm_name                      = "kube2cont01"
-#   vm_ip_address                = "10.1.1.40"
-#   vm_cpu_sockets               = 1
-#   vm_cpu_cores                 = 6
-#   vm_memory                    = 14336
-#   target_node                  = "tear"
-#   template_id                  = 106
-#   ssh_private_key              = var.ssh_private_key
-#   kubernetes_type              = "primary-controller"
-#   kubernetes_cluster_vip       = "10.1.1.11"
-#   kubernetes_pod_subnet        = "10.245.0.0/16"
-#   kubernetes_service_subnet    = "10.97.0.0/16"
-#   kubernetes_api_endpoint_name = "kube2"
-#   make_controller_worker       = true
-#   additional_disk_configurations = [
-#     {
-#       size         = 100
-#       storage_name = "local-lvm"
-#     }
-#   ]
-#   cloudflare_zone_id = var.cloudflare_zone_id
-# }
+module "kube2_controller_primary" {
+  source                       = "./modules/kubernetes-vms"
+  vm_name                      = "kube2cont01"
+  vm_ip_address                = "10.1.1.40"
+  vm_cpu_sockets               = 1
+  vm_cpu_cores                 = 6
+  vm_memory                    = 14336
+  target_node                  = "tear"
+  template_id                  = 106
+  ssh_private_key              = var.ssh_private_key
+  kubernetes_type              = "primary-controller"
+  kubernetes_cluster_vip       = "10.1.1.11"
+  kubernetes_pod_subnet        = "10.245.0.0/16"
+  kubernetes_service_subnet    = "10.97.0.0/16"
+  kubernetes_api_endpoint_name = "kube2"
+  make_controller_worker       = true
+  additional_disk_configurations = [
+    {
+      size         = 100
+      storage_name = "local-lvm"
+    }
+  ]
+  cloudflare_zone_id = var.cloudflare_zone_id
+}
 
-# module "kube2_controller_2" {
-#   source                             = "./modules/kubernetes-vms"
-#   vm_name                            = "kube2cont02"
-#   vm_ip_address                      = "10.1.1.41"
-#   target_node                        = "lefi"
-#   template_id                        = 109
-#   vm_cpu_sockets                     = 1
-#   vm_cpu_cores                       = 6
-#   vm_memory                          = 14336
-#   ssh_private_key                    = var.ssh_private_key
-#   kubernetes_api_endpoint_name       = "kube2"
-#   kubernetes_type                    = "controller"
-#   kubernetes_cluster_token           = var.k8s_cluster_information[1].cluster_token
-#   kubernetes_cluster_certificate_key = var.k8s_cluster_information[1].certificate_key
-#   kubernetes_cacert_hash             = var.k8s_cluster_information[1].cacert_hash
-#   additional_disk_configurations = [
-#     {
-#       size         = 100
-#       storage_name = "local-lvm"
-#     }
-#   ]
-#   make_controller_worker = true
-#   cloudflare_zone_id     = var.cloudflare_zone_id
-# }
+module "kube2_controller_2" {
+  source                             = "./modules/kubernetes-vms"
+  vm_name                            = "kube2cont02"
+  vm_ip_address                      = "10.1.1.41"
+  target_node                        = "lefi"
+  template_id                        = 109
+  vm_cpu_sockets                     = 1
+  vm_cpu_cores                       = 6
+  vm_memory                          = 14336
+  ssh_private_key                    = var.ssh_private_key
+  kubernetes_api_endpoint_name       = "kube2"
+  kubernetes_type                    = "controller"
+  kubernetes_cluster_token           = var.k8s_cluster_information[1].cluster_token
+  kubernetes_cluster_certificate_key = var.k8s_cluster_information[1].certificate_key
+  kubernetes_cacert_hash             = var.k8s_cluster_information[1].cacert_hash
+  additional_disk_configurations = [
+    {
+      size         = 100
+      storage_name = "local-lvm"
+    }
+  ]
+  make_controller_worker = true
+  cloudflare_zone_id     = var.cloudflare_zone_id
+}
 
-# module "kube2_controller_3" {
-#   source                             = "./modules/kubernetes-vms"
-#   vm_name                            = "kube2cont03"
-#   vm_ip_address                      = "10.1.1.42"
-#   target_node                        = "vern"
-#   template_id                        = 112
-#   vm_cpu_sockets                     = 1
-#   vm_cpu_cores                       = 6
-#   vm_memory                          = 14336
-#   ssh_private_key                    = var.ssh_private_key
-#   kubernetes_api_endpoint_name       = "kube2"
-#   kubernetes_type                    = "controller"
-#   kubernetes_cluster_token           = var.k8s_cluster_information[1].cluster_token
-#   kubernetes_cluster_certificate_key = var.k8s_cluster_information[1].certificate_key
-#   kubernetes_cacert_hash             = var.k8s_cluster_information[1].cacert_hash
-#   additional_disk_configurations = [
-#     {
-#       size         = 100
-#       storage_name = "local-lvm"
-#     }
-#   ]
-#   make_controller_worker = true
-#   cloudflare_zone_id     = var.cloudflare_zone_id
-# }
+module "kube2_controller_3" {
+  source                             = "./modules/kubernetes-vms"
+  vm_name                            = "kube2cont03"
+  vm_ip_address                      = "10.1.1.42"
+  target_node                        = "vern"
+  template_id                        = 112
+  vm_cpu_sockets                     = 1
+  vm_cpu_cores                       = 6
+  vm_memory                          = 14336
+  ssh_private_key                    = var.ssh_private_key
+  kubernetes_api_endpoint_name       = "kube2"
+  kubernetes_type                    = "controller"
+  kubernetes_cluster_token           = var.k8s_cluster_information[1].cluster_token
+  kubernetes_cluster_certificate_key = var.k8s_cluster_information[1].certificate_key
+  kubernetes_cacert_hash             = var.k8s_cluster_information[1].cacert_hash
+  additional_disk_configurations = [
+    {
+      size         = 100
+      storage_name = "local-lvm"
+    }
+  ]
+  make_controller_worker = true
+  cloudflare_zone_id     = var.cloudflare_zone_id
+}
 
 
 
