553b60fc7ae790404c9bcf19a4af7c11dc2a4fd1
Alec Hinh <alec4555@gmail.com>
2024-09-07 09:56:37 -0500

    Clean out kube2 nodes
    Signed-off-by: Alec Hinh <alec4555@gmail.com>

--- kube-primary-controllers.tf
+++ kube-primary-controllers.tf
@@ -15,29 +15,3 @@ module "kube_controller_primary" {
   ]
   cloudflare_zone_id = var.cloudflare_zone_id
 }
-
-module "kube2_controller_primary" {
-  source                       = "./modules/kubernetes-vms"
-  vm_name                      = "kube2cont01"
-  vm_ip_address                = "10.1.1.40"
-  vm_cpu_sockets               = 1
-  vm_cpu_cores                 = 6
-  vm_memory                    = 14336
-  make_controller_worker       = true
-  target_node                  = "tear"
-  template_id                  = 106
-  ssh_private_key              = var.ssh_private_key
-  kubernetes_type              = "primary-controller"
-  kubernetes_cluster_vip       = "10.1.1.11"
-  kubernetes_pod_subnet        = "10.245.0.0/16"
-  kubernetes_service_subnet    = "10.97.0.0/16"
-  kubernetes_api_endpoint_name = "kube2"
-  kubernetes_cilium_cluster_id = 200
-  additional_disk_configurations = [
-    {
-      size         = 100
-      storage_name = "local-lvm"
-    }
-  ]
-  cloudflare_zone_id = var.cloudflare_zone_id
-}
--- main.tf
+++ main.tf
@@ -3,50 +3,20 @@
 # and provision the wrong machine with the wrong labels and taints (i.e making a worker node a control plane node)
 # I haven't figured out a way around this yet, but for now its best to just make each node 1 by 1 to be safe.
 
-module "pihole_dns_server_2" {
-  source                    = "./modules/standard-vms/lxc-container"
-  name                      = "pihole02"
-  target_node               = "shizuru"
-  clone_storage             = "local"
-  template_vmid             = "109"
-  ssh_private_key           = var.ssh_private_key
-  ip_address                = "10.1.1.31"
-  cpu_cores                 = 1
-  memory                    = 256
-  root_disk_size            = 12
-  create_dns_record         = false
-  run_standard_provisioning = false
-}
-
-module "captain_minecraft_vm" {
-  source                    = "./modules/standard-vms/lxc-container"
-  name                      = "cap10mc"
-  target_node               = "shizuru"
-  clone_storage             = "local-lvm"
-  template_vmid             = "106"
-  ssh_private_key           = var.ssh_private_key
-  ip_address                = "10.1.1.32"
-  cpu_cores                 = 2
-  memory                    = 4096
-  root_disk_size            = 16
-  cloudflare_zone_id        = var.cloudflare_zone_id
-  run_standard_provisioning = false
-}
-
-module "aqua" {
-  source                    = "./modules/standard-vms/lxc-container"
-  name                      = "aqua"
-  target_node               = "shizuru"
-  clone_storage             = "local"
-  template_vmid             = "110"
-  ssh_private_key           = var.ssh_private_key
-  ip_address                = "10.1.1.16"
-  cpu_cores                 = 1
-  memory                    = 2048
-  root_disk_size            = 12
-  create_dns_record         = false
-  run_standard_provisioning = false
-}
+# module "pihole_dns_server_2" {
+#   source                    = "./modules/standard-vms/lxc-container"
+#   name                      = "pihole02"
+#   target_node               = "shizuru"
+#   clone_storage             = "local"
+#   template_vmid             = "109"
+#   ssh_private_key           = var.ssh_private_key
+#   ip_address                = "10.1.1.31"
+#   cpu_cores                 = 1
+#   memory                    = 256
+#   root_disk_size            = 12
+#   create_dns_record         = false
+#   run_standard_provisioning = false
+# }
 
 module "minio" {
   source             = "./modules/standard-vms/lxc-container"
@@ -58,6 +28,6 @@ module "minio" {
   ip_address         = "10.1.1.17"
   cpu_cores          = 2
   memory             = 4096
-  root_disk_size     = 32
+  root_disk_size     = 100
   cloudflare_zone_id = var.cloudflare_zone_id
 }
