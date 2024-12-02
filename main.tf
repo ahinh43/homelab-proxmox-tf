1dd0b38f14b69350460c2d42dd261a9a197fb0a8
Alec Hinh <alec4555@gmail.com>
2024-12-01 20:57:36 -0600

    Rename user in cloudinit
    Signed-off-by: Alec Hinh <alec4555@gmail.com>

--- main.tf
+++ main.tf
@@ -39,6 +39,8 @@ module "ark_ascended" {
   clone_storage           = "pve"
   ip_address              = "10.1.1.30"
   gateway_address         = "10.1.1.1"
+  cpu_cores = 6
+  memory = (16 * 1024)
   cloudinit_configuration = file("./vm_userdata/ark-ascended.yaml")
   create_dns_record       = true
   cloudflare_zone_id      = var.cloudflare_zone_id
