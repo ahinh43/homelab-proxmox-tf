e56a31b023d8e9feda7f0f7582dba2ad6ba6fa93
Alec Hinh <alec4555@gmail.com>
2026-05-31 15:35:27 -0500

    Add a new dns record
    Signed-off-by: Alec Hinh <alec4555@gmail.com>

--- main.tf
+++ main.tf
@@ -142,3 +142,10 @@ module "homeassistant" {
   create_dns_record       = true
   cloudflare_zone_id      = var.cloudflare_zone_id
 }
+
+module "home_assistant_dns_alias" {
+  source        = "./modules/cloudflare_dns_record"
+  zone_id       = var.cloudflare_zone_id
+  record_name   = "ha"
+  record_target = "10.1.1.39"
+}
